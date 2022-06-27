6e24b15c7636628f81857c4b95773aec8f09d717
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-06-27 17:49:23 +0530

    Implemented comment feature

--- src/controllers.js
+++ src/controllers.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const { createDiv, createParagraph } = require('./html.js');
+
 const homePage = (req, res) => {
   if (req.uri !== '/') {
     res.status(404).send('page not found');
@@ -8,6 +11,7 @@ const homePage = (req, res) => {
 };
 
 const comments = [];
+
 const storeComment = (req, res) => {
   const { queryParams } = req;
   const name = queryParams.get('name');
@@ -16,6 +20,72 @@ const storeComment = (req, res) => {
   comments.push({
     name, comment, timestamp: new Date()
   });
+
+  res.redirect('/guest-book');
 };
 
-module.exports = { homePage };
+const getTime = (date) => {
+  return {
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
+  };
+};
+
+const getDate = (date) => {
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth(),
+    day: date.getDate()
+  };
+};
+
+const formateDate = (date) => {
+  const { hours, minutes, seconds } = getTime(date);
+  const { year, month, day } = getDate(date);
+
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+  const formattedDate = `${day}-${month}-${year}`;
+
+  return `${formattedTime} | ${formattedDate}`;
+};
+
+const createCommentHtml = ({ name, timestamp, comment }) => {
+  const author = createParagraph({
+    content: name,
+    classes: ['author']
+  });
+
+  const commentText = createParagraph({
+    content: comment,
+    classes: ['comment-text']
+  });
+
+  const time = createParagraph({
+    content: formateDate(timestamp),
+    classes: ['timestamp']
+  });
+
+  return createDiv({
+    content: `${author}${commentText}${time}`,
+    classes: ['comment']
+  });
+};
+
+const guestBook = (req, res) => {
+  fs.readFile('./public/guest_book.html', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Some error occurred, please try again!');
+      return;
+    }
+
+    const commentsHtml = comments.slice(0).reverse().map(comment => {
+      return createCommentHtml(comment);
+    }).join('');
+
+    const newGuestBook = data.replace('__COMMENTS__', commentsHtml);
+    res.sendHtml(newGuestBook);
+  });
+};
+
+module.exports = { homePage, storeComment, guestBook };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('./router.js');
 const { serveFile } = require('./serveFiles.js');
-const { homePage } = require('./controllers.js');
+const { homePage, storeComment, guestBook } = require('./controllers.js');
 
 const router = new Router();
 
 router.get('/public', serveFile);
+
+router.get('/guest-book', guestBook);
+
+router.get('/comment', storeComment);
 
 router.get('/', homePage);
 
