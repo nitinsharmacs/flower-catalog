ffac1a4cf2f0ec902bb4662599033fbb4df9d5c7
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-06-27 16:56:16 +0530

    Created html files

--- src/controllers.js
+++ src/controllers.js
@@ -1,9 +1,21 @@
 const homePage = (req, res) => {
   if (req.uri !== '/') {
     res.status(404).send('page not found');
+    return;
   }
 
   res.redirect('/public/index.html');
+};
+
+const comments = [];
+const storeComment = (req, res) => {
+  const { queryParams } = req;
+  const name = queryParams.get('name');
+  const comment = queryParams.get('comment');
+
+  comments.push({
+    name, comment, timestamp: new Date()
+  });
 };
 
 module.exports = { homePage };
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const { homePage } = require('./controllers.js');
 const router = new Router();
 
 router.get('/public', serveFile);
+
 router.get('/', homePage);
 
 module.exports = { router };
